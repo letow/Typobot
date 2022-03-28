e0dc4cd62c4d32f3da70affdeffc602bf77db87b
George <letow@vk.com>
2022-03-28 19:36:03 +0500

    Fixed a bug with "next line"

--- typograf.py
+++ typograf.py
@@ -8,20 +8,23 @@ bot = telebot.TeleBot(token.read())
 token.close()
 
 edited_text = ''
-
+'''
 class MyHTMLParser(HTMLParser):
     def handle_data(self, data):
         global edited_text
         edited_text = data
 parser = MyHTMLParser()
+'''
 
 @bot.channel_post_handler(content_types=['text'])
 def typograf(post):
-    global parser
+    #global parser
     global edited_text
     unedited_text = post.text
-    response = requests.post('https://www.typograf.ru/webservice/', data={'text': unedited_text, 'chr': 'UTF-8'})
-    parser.feed(html.unescape(str(response.content, encoding='utf-8')))
+    response = requests.post('https://www.typograf.ru/webservice/', data={'text': r'{}'.format(unedited_text), 'chr': 'utf-8', 'xml':'''<?xml version="1.0" encoding="windows-1251" ?><preferences><tags delete="2">1</tags></preferences>'''})
+    #parser.feed(html.unescape(str(response.content, encoding='utf-8')))
+    edited_text = html.unescape(str(response.content, encoding='utf-8'))
     bot.edit_message_text(edited_text, post.chat.id, post.id)
+
 
 bot.infinity_polling()
